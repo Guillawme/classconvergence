92b49502d8f113e56632a05a24d1560b5d314d19
Guillaume Gaullier <guillaume@gaullier.org>
2021-01-22 16:31:15 +0100

    Added function to count particles in each class

--- classconvergence.py
+++ classconvergence.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import starfile as star
+import pandas as pd
 
 # Building blocks
 
@@ -15,3 +16,9 @@ def load_star_files(directory, pattern):
         full_files = [ directory + '/' + file for file in files ]
         dfs = [ star.open(file) for file in full_files if pattern in file ]
         return dfs
+
+def count_particles(particles):
+    """Count particles in each class."""
+    particle_counts = particles[['rlnClassNumber']].value_counts()
+    fractions_of_total = particles[['rlnClassNumber']].value_counts(normalize=True) * 100
+    return pd.DataFrame({'count': particle_counts, 'fraction': fractions_of_total})
