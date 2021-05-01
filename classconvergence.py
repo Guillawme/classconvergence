4a8e5ae2d8959b975a41b6802321429eb6eaf6d1
Guillaume Gaullier <guillaume@gaullier.org>
2021-05-01 13:18:16 +0200

    Better handling of wrong inputs

--- classconvergence.py
+++ classconvergence.py
@@ -8,13 +8,16 @@ import pandas as pd
 def load_star_files(directory, pattern):
     """Load star files matching a given pattern from a given directory."""
     if not os.path.isdir(directory):
-        sys.stderr.write("Error: the given path is not a directory.")
+        sys.stderr.write(f'Error: "{directory}" is not a directory.')
         sys.exit(1)
     else:
         files = os.listdir(directory)
         files.sort()
         full_files = [ directory + '/' + file for file in files ]
         dfs = [ star.open(file) for file in full_files if pattern in file ]
+        if len(dfs) == 0:
+            sys.stderr.write(f'No files matching pattern "{pattern}" could be found in "{directory}".')
+            sys.exit(1)
         return dfs
 
 def count_particles(particles):
